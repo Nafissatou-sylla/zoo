3b72b99767e59c107e07b2a6b84377e11b974509
Sylla <finasylla2020@gmail.com>
2022-10-13 21:43:41 +0200

    class Cage

---
+++ src/main/java/org/formation/sylla/modelisationdezoo/controler/Cage.java
@@ -0,0 +1,36 @@
+package org.formation.sylla.modelisationdezoo.controler;
+
+
+public class Cage {
+	private Animal animal;
+	
+	public Cage() {}
+
+	
+	/**
+	 * @param cages
+	 */
+	public Cage(Animal animal) {
+		this.animal = animal;
+	}
+
+
+
+	/**
+	 * @return the animal
+	 */
+	public Animal getAnimal() {
+		return animal;
+	}
+
+
+	/**
+	 * @param animal the animal to set
+	 */
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+
+
+
+}
