b17e36b87c60a44d4433c0026d122261c9c479d9
Sylla <finasylla2020@gmail.com>
2022-10-13 09:59:22 +0200

    class gazelle

---
+++ src/main/java/org/formation/sylla/modelisationdezoo/controler/Gazelle.java
@@ -0,0 +1,29 @@
+package org.formation.sylla.modelisationdezoo.controler;
+
+public class Gazelle extends Animal{
+	private double hornLenght;
+	
+	public Gazelle(String name, int age, double weight, double hornLenght) {
+		super(name, age, weight);
+		this.setHornLenght(hornLenght);
+	}
+
+	/**
+	 * @return the hornLenght
+	 */
+	public double getHornLenght() { return hornLenght; }
+
+	/**
+	 * @param hornLenght the hornLenght to set
+	 */
+	public void setHornLenght(double hornLenght) { this.hornLenght = hornLenght; }
+
+	@Override
+	public void eat() { this.grow(0.4); }
+
+	@Override
+	public void sleep() { this.losingWeight(0.1); }
+
+	public void jump() { this.losingWeight(0.07); }
+
+}
