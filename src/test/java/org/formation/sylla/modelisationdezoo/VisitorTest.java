62cc64cbb631b1d42a92a6e653bb67a25ee7a922
Sylla <finasylla2020@gmail.com>
2022-10-15 23:57:40 +0200

    class VisitorTest

--- src/test/java/org/formation/sylla/modelisationdezoo/VisitorTest.java
+++ src/test/java/org/formation/sylla/modelisationdezoo/VisitorTest.java
@@ -1,10 +1,6 @@
 package org.formation.sylla.modelisationdezoo;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.formation.sylla.modelisationdezoo.manager.Animal;
-import org.formation.sylla.modelisationdezoo.manager.Cage;
-import org.formation.sylla.modelisationdezoo.manager.Lion;
 import org.formation.sylla.modelisationdezoo.manager.Visitor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
